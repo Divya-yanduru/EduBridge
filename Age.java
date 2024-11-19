27a8336c62a908b03d73cfd008a848b63fa53b28
Divya-yanduru <divyayanduru@gmail.com>
2024-11-19 16:14:46 +0530

    added

---
+++ Age.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Age {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		long number;
+		Scanner scanner=new Scanner(System.in);
+	    number=scanner.nextLong();
+	    if(number>1000)
+	    {
+	    	System.out.println("number is greater than 1000");
+	    }else
+	    {
+	    	System.out.println("number is not greater than 1000");
+	    }
+	   
+		
